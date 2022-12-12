ec75b09f0e17c27a5c7325047997c477534a9c53
Barry-2020 <manuel14118@gmail.com>
2022-12-12 02:03:42 -0500

    lista de usuarios completa plin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -44,35 +44,47 @@ class UserSeeder extends Seeder
             ['name' => 'MAMANI GONZALO JHOSELYN YENIDA', 'email' => '76843472', 'password' => bcrypt('180184')],
             ['name' => 'PARI ILLANES KAREN ANYELA', 'email' => '70111032', 'password' => bcrypt('174832')],
             ['name' => 'VENTURA CONDORI CLINTON KENEDY', 'email' => '74947760', 'password' => bcrypt('181134')],
+            ['name' => 'CHURA CHURA OLINDA DANITZA', 'email' => '71824090', 'password' => bcrypt('181906')],
+            ['name' => 'CONDORI MAYHUA IVAN ESMIT', 'email' => '70476967', 'password' => bcrypt('121889')],
+            ['name' => 'CUSI JUSTO CESAR CLAUDIO', 'email' => '73033039', 'password' => bcrypt('181759')],
+            ['name' => 'FLORES ILACOPA LEIDA IDALECIA', 'email' => '74651116', 'password' => bcrypt('181185')],
+            ['name' => 'GOMEZ ALANOCA JOEL SANTOS', 'email' => '71052981', 'password' => bcrypt('181895')],
+            ['name' => 'GUZMAN TULA JOSE EDUARDO', 'email' => '76138395', 'password' => bcrypt('181093')],
+            ['name' => 'HUANCA GUERRA JOHN CARLOS', 'email' => '70756159', 'password' => bcrypt('141191')],
+            ['name' => 'LEON CHIPANA WILLIAM WILBER', 'email' => '71657437', 'password' => bcrypt('181936')],
+            ['name' => 'LUQUE CANAZA REYKER YAXKIN', 'email' => '74171273', 'password' => bcrypt('181918')],
+            ['name' => 'MAMANI LOPEZ, JULIO ELIAS', 'email' => '72471491', 'password' => bcrypt('174386')],
+            ['name' => 'MULLISACA JAEN HECTOR ARTURO', 'email' => '72384062', 'password' => bcrypt('180186')],
+            ['name' => 'RAMOS HALANOCA CRISTIAN REYSCELL', 'email' => '72872001', 'password' => bcrypt('180583')],
+            ['name' => 'ROSAS CEREZO LUIS MIGUEL', 'email' => '72737934', 'password' => bcrypt('102123')],
+            ['name' => 'SALAZAR CUTIPA JUAN MIJAEL', 'email' => '74835757', 'password' => bcrypt('181143')],
+            ['name' => 'TALIZO CHAMBILLA MIGUEL ANGEL', 'email' => '70977160', 'password' => bcrypt('181163')],
+            ['name' => 'TAPARA CANSAYA DENNIS HENRY', 'email' => '70817529', 'password' => bcrypt('174390')],
+            ['name' => 'YANCAPALLO MANUEL JOEL MARCOS', 'email' => '48157560', 'password' => bcrypt('122128')],
         ];
 
 
         foreach($users as $user){
             User::create($user);
         }
+        //
 
+        //FALTAAAAAAA
         // 171663	73525858	CAMPOS ALVAREZ LIZETH
         // 171088	70221727	CCORI ARAGON ERICK GONZALO
         // 092318	70244547	CHAIÑA DEL CASTILLO JHERSSON ERNESTO
         // 174864	71430477	CHARCA QUISPE RONAL
         // 153154	70269453	CHOQUEHUANCA CACERES WILIAN ADRIAN
-        // 181906	71824090	CHURA CHURA OLINDA DANITZA
+        //
         // 120285	71046678	CHURA CRUZ ROY MODES
         // 140832	72865330	COILA TICONA JEHAN CARLOS
-        // 181759	73033039	CUSI JUSTO CESAR CLAUDIO
         // 154159	78378359	ESPINOZA CASTILLO EDHER
-        // 181185	74651116	FLORES ILACOPA LEIDA IDALECIA
-        // 181895	71052981	GOMEZ ALANOCA JOEL SANTOS
-        // 181093	76138395	GUZMAN TULA JOSE EDUARDO
-        // 141191	70756159	HUANCA GUERRA JOHN CARLOS
         // 174389	76233005	HUARAYA AMANQUI SILVANA YANETH
         // 170632	73744393	HUAYNAPATA UCHARICO FREDDY WALTER
         // 163111	71027461	INCACUTIPA HUARACHI WALTER BERNABE
-        // 181936	71657437	LEON CHIPANA WILLIAM WILBER
         // 164456	76541031	LIMA CECENARDO JESUS ORLANDO
 
 
-        // 174386	72471491	MAMANI LOPEZ, JULIO ELIAS
         // 174402	73777406	MAMANI SONCCO LISSBETH RUDY
         // 174401	77168808	MAQUERA ARACA LUZ PILAR
         // 135461	45241201	MERMA VILCA YESSICA VERONICA
@@ -84,19 +96,13 @@ class UserSeeder extends Seeder
         // 142845	73352473	QUISPE MAMANI YONY SADAN
         // 170237	74600166	QUISPE QUISPE WALDIR RIVALDO
 
-        // 180583	72872001	RAMOS HALANOCA CRISTIAN REYSCELL
-        // 102123	72737934	ROSAS CEREZO LUIS MIGUEL
         // 160125	74624281	SACARI HUERTA WILSON RENE
-        // 181143	74835757	SALAZAR CUTIPA JUAN MIJAEL
         // 134286	73651882	SANGA CHAHUA JOSE FREDY
         // 160403	74074397	SARDON CUTIPA CARLOS ROBERTO
 
-        // 181163	70977160	TALIZO CHAMBILLA MIGUEL ANGEL
-        // 174390	70817529	TAPARA CANSAYA DENNIS HENRY
 
 
         // 162041	77381979	VILLASANTE DEL CARPIO MARCELO ATILIO
-        // 122128	48157560	YANCAPALLO MANUEL JOEL MARCOS
         // 171218	71568784	YUCRA MENDOZA ANGEL GABRIEL
 
     }
